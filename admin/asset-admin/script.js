5133fe2abab7d11305ab3feb2f4866a5c3689f3d
AdeRiky <Adhericky67@gmail.com>
2023-10-10 14:14:21 +0700

    update

--- admin/asset-admin/script.js
+++ admin/asset-admin/script.js
@@ -14,3 +14,52 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+const orderForm = document.getElementById("orderForm");
+const alertMessage = document.getElementById("alertMessage");
+
+orderForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const formData = {
+        no_resi: document.getElementById("no_resi").value,
+        fee: parseInt(document.getElementById("fee").value) || null,
+        senderName: document.getElementById("senderName").value,
+        status: document.getElementById("status").value,
+        senderPhone: document.getElementById("senderPhone").value,
+        senderaddress: document.getElementById("senderaddress").value,
+        receiptName: document.getElementById("receiptName").value,
+        receiptPhone: document.getElementById("receiptPhone").value,
+        receiptaddress: document.getElementById("receiptaddress").value,
+        estimatedTimeOfDeparture: document.getElementById("estimatedTimeOfDeparture").value,
+        estimatedTimeOfArrival: document.getElementById("estimatedTimeOfArrival").value || null,
+        adminName: document.getElementById("adminName").value,
+    };
+
+    // Kirim data ke API menggunakan metode POST
+    fetch("https://gleaming-tuna-ring.cyclic.app/api/order", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Data berhasil ditambahkan:", data);
+            // Tampilkan pesan sukses
+            alertMessage.innerText = "Data berhasil ditambahkan!";
+            alertMessage.style.backgroundColor = "#4CAF50";
+            alertMessage.style.display = "block";
+
+            // Redirect atau tampilkan pesan sukses
+            setTimeout(function () {
+                window.location.href = "index.html";
+            }, 2000); // Arahkan kembali setelah 2 detik
+        })
+        .catch((error) => {
+            console.error("Gagal menambahkan data:", error);
+            // Tampilkan pesan error
+        });
+});
